d8fe3f0a9945ff31e33c76321ba2f164615a7871
Nicolas <nico@rottenbytes.info>
2010-09-29 23:06:17 +0200

    added basic caching to facts

--- lib/mcollective/facts/base.rb
+++ lib/mcollective/facts/base.rb
@@ -10,13 +10,30 @@ module MCollective
             # Registers new fact sources into the plugin manager
             def self.inherited(klass)
                 PluginManager << {:type => "facts_plugin", :class => klass.to_s}
+
+                @@fact_timestamp = Time.now.to_i
+                @@facts={}
+                @@cachetime = Config.instance.pluginconf["facts.cachetime"].to_i
+                Log.instance.debug("Initializing fact cache timestamp (#{@@fact_timestamp})")
+                Log.instance.debug("Initializing fact cache to nil")
+                Log.instance.debug("Caching facts for #{@@cachetime} seconds")
             end
 
             # Returns the value of a single fact
             def get_fact(fact)
-                facts = get_facts
-
-                facts.include?(fact) ? facts[fact] : nil
+                # outdated cache / no initialized facts
+                Log.instance.debug("cache analysis : #{@@fact_timestamp} / Actual : " + Time.now.to_i.to_s)
+                if ((Time.now.to_i - @@fact_timestamp.to_i).to_i > @@cachetime) or @@facts.empty? then
+                    # update facts
+                    Thread.exclusive do
+                       Log.instance.debug("Outdated/uninitialized facts, updating")
+                       @@facts=get_facts
+                       # update timestamp
+                       @@fact_timestamp = Time.now.to_i
+                    end
+                end
+            
+                @@facts.include?(fact) ? @@facts[fact] : nil
             end
 
             # Returns true if we know about a specific fact, false otherwise
@@ -25,6 +42,8 @@ module MCollective
 
                 facts.include?(fact)
             end
+            
+            
         end
     end
 end
