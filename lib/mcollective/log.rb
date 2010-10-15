ba93b895cc865c1d241f598e0e21595b7577b190
Nico <nico@rottenbytes.info>
2010-10-15 14:18:27 +0200

    refactored some parts / added cycling for all mechanisms

--- lib/mcollective/log.rb
+++ lib/mcollective/log.rb
@@ -5,10 +5,24 @@ module MCollective
 
         @logger = nil
 
+        # A bit of meta programmation to reduce code duplication
+        def self.log_at(name)
+            define_method(name) { |msg| 
+                log(@loglevel, msg)
+            }
+        end
+        
+        levels = ["info", "warn", "debug", "fatal", "error"]
+        levels.each { |level|
+            log_at level.to_sym
+        }
+
+
         def initialize
             config = Config.instance
             raise ("Configuration has not been loaded, can't start logger") unless config.configured
             @logmechanism = config.logmechanism
+            @loglevel = map_level(config.loglevel)
             
             case @logmechanism 
                 when "syslog"
@@ -18,137 +32,81 @@ module MCollective
                     rescue RuntimeError
                         # sometimes already opened, ruby syslog bug ?
                     end
-                    # loglevel has to be a valid syslog level
-                    valid_levels = ["crit", "emerg", "alert", "warning", "notice", "info", "debug", "err"]
-                    raise("Loglevel should be a valid syslog level") unless valid_levels.include? config.loglevel
+                    # loglevel has to be a valid syslog level (maybe remove since map_level ensures integrity ?)
+                    valid_levels = ["crit", "warning", "info", "debug", "err"]
+                    raise("Loglevel should be a valid syslog level (#{@loglevel} passed)") unless valid_levels.include? @loglevel
                     
                 when "logger"
                     @logger = Logger.new(config.logfile, config.keeplogs, config.max_log_size)
                     @logger.formatter = Logger::Formatter.new
                     
-                    case config.loglevel
-                        when "info"
-                            @logger.level = Logger::INFO
-                        when "warn"
-                            @logger.level = Logger::WARN
-                        when "debug"
-                            @logger.level = Logger::DEBUG
-                        when "fatal"
-                            @logger.level = Logger::FATAL
-                        when "error"
-                            @logger.level = Logger::ERROR
-                        else
-                            @logger.level = Logger::INFO
-                            error("Invalid log level #{config.loglevel}, defaulting to info")
-                    end
+                    @logger.level =@loglevel
                 else
                     raise("Invalid log mechanism !")
                 end
         end
-
+        
         def finalize
             # For sanity (even if finalize may not be always called)
             if @logmechanism == "syslog"
                 @logger.close
             end
-        end
+        end 
+
+        def map_level(level)
+            levels = ["info", "warn", "debug", "fatal", "error"]
+            logger_levels = [ Logger::INFO, Logger::WARN, Logger::DEBUG, Logger::FATAL, Logger::ERROR ]
+            syslog_levels = [ "info", "warning", "debug", "crit", "err" ]
+            
+            case @logmechanism
+                when "syslog"
+                    return syslog_levels[levels.index level]
+                when "logger"
+                    return logger_levels[levels.index level]
+            end
+        end 
 
         # cycles the log level increasing it till it gets to the highest
         # then down to the lowest again
         def cycle_level
-            # FIXME : need rework/refactor to be done with syslog too
-            if @logmechanism == "syslog"
-                config = Config.instance
-
-                case @logger.level
-                    when Logger::FATAL
-                        @logger.level = Logger::ERROR
-                        error("Logging level is now ERROR configured level is #{config.loglevel}")
-
-                    when Logger::ERROR
-                        @logger.level = Logger::WARN
-                        warn("Logging level is now WARN configured level is #{config.loglevel}")
-
-                    when Logger::WARN
-                        @logger.level = Logger::INFO
-                        info("Logging level is now INFO configured level is #{config.loglevel}")
-
-                    when Logger::INFO
-                        @logger.level = Logger::DEBUG
-                        info("Logging level is now DEBUG configured level is #{config.loglevel}")
-
-                    when Logger::DEBUG
-                        @logger.level = Logger::FATAL
-                        fatal("Logging level is now FATAL configured level is #{config.loglevel}")
-
+            levels = ["info", "warn", "debug", "fatal", "error"]
+        
+            case @logmechanism
+                when "syslog"
+                    syslog_levels = [ "info", "warning", "debug", "crit", "err" ]
+                    old_level = syslog_levels.index @loglevel
+                    
+                    if ((syslog_levels.index @loglevel) +1 < syslog_levels.length)
+                        @loglevel = syslog_levels[(syslog_levels.index(@loglevel) +1)]
                     else
-                        @logger.level = Logger::DEBUG
-                        info("Logging level now DEBUG configured level is #{config.loglevel}")
-                end
+                        @loglevel = syslog_levels[0]
+                    end
+                    log(@loglevel,"Switching loglevel from #{levels[old_level]} to #{@loglevel}")
+                when "logger"
+                    logger_levels = [ Logger::INFO, Logger::WARN, Logger::DEBUG, Logger::FATAL, Logger::ERROR ]
+                    old_level = logger_levels.index @loglevel
+                    
+                    if ((logger_levels.index @loglevel) +1 < logger_levels.length)
+                        @loglevel = logger_levels[(logger_levels.index(@loglevel) +1)]
+                    else
+                        @loglevel = logger_levels[0]
+                    end
+                    log(@loglevel,"Switching loglevel from #{levels[old_level]} to #{logger_levels[@loglevel]}")
             end
         end
-
-        ######## FIXME #########
-        # Refactor this too
-        ########################
-        
-        # logs at level INFO
-        def info(msg)
-            case @logmechanism
-                when "syslog"
-                    Syslog.info(msg)
-                when "logger"
-                    log(Logger::INFO, msg)
-                end
-        end
-
-        # logs at level WARN
-        def warn(msg)
-            case @logmechanism
-                when "syslog"
-                    Syslog.warning(msg)
-                when "logger"
-                    log(Logger::WARN, msg)
-                end
-        end
-
-        # logs at level DEBUG
-        def debug(msg)
-            case @logmechanism
-                when "syslog"
-                    Syslog.debug(msg)
-                when "logger"
-                    log(Logger::DEBUG, msg)
-                end
-        end
-
-        # logs at level FATAL - looks like fatal is not available in syslog ?!?
-        def fatal(msg)
-            case @logmechanism
-                when "syslog"
-                    Syslog.crit(msg)
-                when "logger"
-                    log(Logger::FATAL, msg)
-                end
-        end
-
-        # logs at level ERROR
-        def error(msg)
-            case @logmechanism
-                when "syslog"
-                    Syslog.err(msg)
-                when
-                    log(Logger::ERROR, msg)
-                end
-        end
-
+                 
         private
         # do some fancy logging with caller information etc
         def log(severity, msg)
-            begin
-                from = File.basename(caller[1])
-                @logger.add(severity) { "#{$$} #{from}: #{msg}" }
-            rescue Exception => e
+            case @logmechanism
+                when "logger"
+                    begin
+                        from = File.basename(caller[1])
+                        @logger.add(severity) { "#{$$} #{from}: #{msg}" }
+                    rescue Exception => e
+                    end
+                when "syslog"
+                    Syslog.send(severity, msg)
             end
         end
     end
